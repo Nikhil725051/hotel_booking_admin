fc45f7e809e8d898b977abf9b8f32e6b8b5b9534
Nikhil Kumar <nikhil725051@gmail.com>
2022-11-08 15:06:46 +0530

    cardWidget finished

--- src/components/cardWidget/CardWidget.js
+++ src/components/cardWidget/CardWidget.js
@@ -1,28 +1,100 @@
 import './cardWidget.scss';
-import {AccountBalanceWalletOutlined, KeyboardArrowUp} from '@mui/icons-material';
+import {
+    AccountBalanceWalletOutlined,
+    KeyboardArrowUp,
+    PersonOutlineOutlined,
+    ShoppingCartOutlined,
+    MonetizationOnOutlined,
+} from '@mui/icons-material';
 
 export default function CardWidget({type}){
 
     var data;
 
-    switch(type){
+    switch (type) {
         case "users":
             data = {
-                
+                title: "Users",
+                counter: "100",
+                cardAction: "See all users",
+                progress: 20,
+                icon: <PersonOutlineOutlined
+                 className='icon'
+                 style={
+                    {
+                        color: "red",
+                        backgroundColor: "rgb(255 0 0 / 26%)" 
+                    }
+                 }>
+                 </PersonOutlineOutlined>
             }
+            break;
+        case "orders":
+            data = {
+                title: "Orders",
+                counter: "100",
+                cardAction: "View all orders",
+                progress: 20,
+                icon: <ShoppingCartOutlined
+                 className='icon'
+                 style={
+                    {
+                        color: "gold",
+                        backgroundColor: "#ffd7004f" 
+                    }
+                 }>
+                 </ShoppingCartOutlined>
+            }
+            break;
+        case "earnings":
+            data = {
+                title: "Earnings",
+                counter: "$100",
+                cardAction: "View net earnings",
+                progress: 20,
+                icon: <MonetizationOnOutlined
+                 className='icon'
+                 style={
+                    {
+                        color: "green",
+                        backgroundColor: "rgba(0, 128, 0, 0.315)" 
+                    }
+                 }>
+                 </MonetizationOnOutlined>
+            }
+            break;
+        case "balance":
+            data = {
+                title: "Balance",
+                counter: "$100",
+                cardAction: "See details",
+                progress: 20,
+                icon: <AccountBalanceWalletOutlined
+                 className='icon'
+                 style={
+                    {
+                        color: "purple",
+                        backgroundColor: "rgba(128, 0, 128, 0.375)" 
+                    }
+                 }
+                 >
+                 </AccountBalanceWalletOutlined>
+            }
+            break;
     }
     return(<div className='card'>
-        <div className='cardItem'>
-            <span className='title'>Users</span>
-            <div className='progress'>
-                <KeyboardArrowUp></KeyboardArrowUp>
-                <span>20%</span>
-            </div>
+        <div className='cardLeft'>
+            <span className='title'>{data.title}</span>
+            <span className='counter'>{data.counter}</span>
+            <span className='cardAction'>{data.cardAction}</span>
         </div>
-        <span className='cardItem quantity'>100</span>
-        <div className='cardItem'>
-            <span>See all users</span>
-            <AccountBalanceWalletOutlined className='icon'></AccountBalanceWalletOutlined>
+       
+        <div className='cardRight'>
+           <div className='progress'>
+                <KeyboardArrowUp></KeyboardArrowUp>
+                <span>{data.progress}%</span>
+            </div>
+            {data.icon}
         </div>
     </div>);
 }
