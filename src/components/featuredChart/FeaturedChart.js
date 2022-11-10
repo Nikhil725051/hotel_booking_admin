145d2a29d8788dfc28fe712d8b8f5b80ac4eee95
Nikhil Kumar <nikhil725051@gmail.com>
2022-11-10 18:11:55 +0530

    Added simple chart

--- src/components/featuredChart/FeaturedChart.js
+++ src/components/featuredChart/FeaturedChart.js
@@ -1,5 +1,6 @@
-import { MoreVertOutlined } from '@mui/icons-material';
+import { KeyboardArrowDown, MoreVertOutlined } from '@mui/icons-material';
 import {CircularProgressbar} from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 import './featuredChart.scss'
 
 
@@ -15,7 +16,22 @@ export default function FeaturedChart(){
         <div className='progressBar'>
           <CircularProgressbar value={percentage} text={`${percentage}%`}></CircularProgressbar>
           <h3 className='chartText'>Total sales made today</h3>
-          <h2>$420</h2>
+          <h1 className='totalSales'>$420</h1>
+        </div>
+        <p className='transactionPara'>Previous transactions processing. Last payments may not be included</p>
+        <div className='statsWrapper'>
+            <div className='stats'>
+                <h3 className='statsTitle'>Target</h3>
+                <span><KeyboardArrowDown></KeyboardArrowDown>$12.4k</span>
+            </div>
+            <div className='stats'>
+                <h3 className='statsTitle'>Last week</h3>
+                <span><KeyboardArrowDown></KeyboardArrowDown>$12.4k</span>
+            </div>
+            <div className='stats'>
+                <h3 className='statsTitle'>Last month</h3>
+                <span><KeyboardArrowDown></KeyboardArrowDown>$12.4k</span>
+            </div>
         </div>
     </div>);
 }
